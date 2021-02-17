dcd6bba499e6562567e46ef126bf4e21976ff3b1
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 12:47:59 +0000

    Create 07CakeIngredients.cs

---
+++ 02ProgrammingFundamentals_CSharpConditionalStatementsAndLoopsExercise/07CakeIngredients.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace _07CakeIngredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ingredientsCount = 0;            
+            while (true) 
+            {
+                string ingredient = Console.ReadLine();
+                if (ingredient == "Bake!") break;
+                Console.WriteLine($"Adding ingredient {ingredient}.");
+                ingredientsCount++;
+            }
+            Console.WriteLine($"Preparing cake with {ingredientsCount} ingredients.");
+        }
+    }
+}
