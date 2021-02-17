efc4c2c61df7b4ed02b9cc0de4d8b8de4fa3c186
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 16:46:42 +0000

    Create 06CalculateTriangleArea.cs

---
+++ 05ProgrammingFundamentals_MethodsLab/06CalculateTriangleArea.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace _06CalculateTriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double area = GetTriangleArea(a, h);
+            Console.WriteLine(area);
+        }
+
+        static double GetTriangleArea(double width, double height)
+        {
+            return (width * height) / 2;
+        }
+    }
+}
