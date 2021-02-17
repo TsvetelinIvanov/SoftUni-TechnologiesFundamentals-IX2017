e83c35fcbea006cda76bdcada17ff990bccf6ded
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 14:34:13 +0000

    Create 03ExactSumOfRealNumbers.cs

---
+++ 04ProgrammingFundamentals_DataTypesAndVariablesLab/03ExactSumOfRealNumbers.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace _03ExactSumOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0m;
+            for (int i = 0; i < n; i++)
+            {
+                decimal m = decimal.Parse(Console.ReadLine());
+                sum += m;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
