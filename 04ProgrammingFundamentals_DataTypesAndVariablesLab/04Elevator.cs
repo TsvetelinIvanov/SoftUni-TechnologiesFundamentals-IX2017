a2fc400b7f72e0726a3da767857499dbafd6d521
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 14:36:41 +0000

    Create 04Elevator.cs

---
+++ 04ProgrammingFundamentals_DataTypesAndVariablesLab/04Elevator.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace _04Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            int courses = (int)Math.Ceiling((double)n / p);
+            Console.WriteLine(courses);
+        }
+    }
+}
