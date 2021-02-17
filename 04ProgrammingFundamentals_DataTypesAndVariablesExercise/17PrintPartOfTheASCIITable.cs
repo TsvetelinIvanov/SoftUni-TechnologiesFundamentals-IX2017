434e8fb9bef63f3c4b4d2741eb36551aa6a2299e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 15:53:32 +0000

    Create 17PrintPartOfTheASCIITable.cs

---
+++ 04ProgrammingFundamentals_DataTypesAndVariablesExercise/17PrintPartOfTheASCIITable.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace _17PrintPartOfTheASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstChar = int.Parse(Console.ReadLine());
+            int lastChar = int.Parse(Console.ReadLine());
+
+            for (char i = (char)firstChar; i <= (char)lastChar; i++)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
