d2932b4c7270af415b98d161f5449bd365be1391
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 09:50:12 +0000

    Create 02RectangleArea.cs

---
+++ 01ProgrammingFundamentals_CSharpIntroductionAndBasicSyntax/02RectangleArea.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace _02RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double area = width * height;
+            Console.WriteLine("{0:f2}", area);
+        }
+    }
+}
