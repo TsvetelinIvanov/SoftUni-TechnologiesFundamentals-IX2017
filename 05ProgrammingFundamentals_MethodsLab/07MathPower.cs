54f6c5f964af70865452d3fe577349b71c44293b
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 16:51:02 +0000

    Create 07MathPower.cs

---
+++ 05ProgrammingFundamentals_MethodsLab/07MathPower.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace _07MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            double result = RaiseToPower(number, power);
+            Console.WriteLine(result);
+        }
+
+        static double RaiseToPower(double number, int power)
+        {
+            double result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= number;
+            }
+            return result;
+        }
+    }
+}
