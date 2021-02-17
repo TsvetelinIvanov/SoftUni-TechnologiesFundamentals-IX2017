9ef5b6fc632cd3210d528d6ef86ade36048e3dee
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 10:07:11 +0000

    Create 03MilesToKilometers.cs

---
+++ 01ProgrammingFundamentals_CSharpIntroductionAndBasicSyntax/03MilesToKilometers.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace _03MilesToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miles = double.Parse(Console.ReadLine());
+            double kilometers = miles * 1.60934;
+            Console.WriteLine("{0:f2}", kilometers);
+        }
+    }
+}
