6b2a25c3423948c5ab924b4abdd7236dd19768b5
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 12:44:11 +0000

    Create 06IntervalOfNumbers.cs

---
+++ 02ProgrammingFundamentals_CSharpConditionalStatementsAndLoopsExercise/06IntervalOfNumbers.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace _06IntervalOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int startNumber = Math.Min(firstNumber, secondNumber);
+            int endNumber = Math.Max(firstNumber, secondNumber);
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
