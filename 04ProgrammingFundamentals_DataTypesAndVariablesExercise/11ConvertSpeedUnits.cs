801e4fad63f03e6a3c4b536466a4ad583978af7d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 15:25:59 +0000

    Create 11ConvertSpeedUnits.cs

---
+++ 04ProgrammingFundamentals_DataTypesAndVariablesExercise/11ConvertSpeedUnits.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace _11ConvertSpeedUnits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float meters = float.Parse(Console.ReadLine());
+            float hours = float.Parse(Console.ReadLine());
+            float minutes = float.Parse(Console.ReadLine());
+            float seconds = float.Parse(Console.ReadLine());
+            float kilometers = meters / 1000;
+            float miles = meters / 1609;
+
+            float metersPerSecond = meters / ((3600 * hours) + (60 * minutes) + seconds);
+            float kilometersPerHour = kilometers / (hours + (minutes / 60) + (seconds / 3600));
+            float milesPerHour = miles / (hours + (minutes / 60) + (seconds / 3600));
+
+            Console.WriteLine(metersPerSecond);
+            Console.WriteLine(kilometersPerHour);
+            Console.WriteLine(milesPerHour);
+        }
+    }
+}
