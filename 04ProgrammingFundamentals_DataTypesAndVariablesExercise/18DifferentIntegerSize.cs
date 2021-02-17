c3e9816e800e990bb9b26f346894c76b1091e529
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 15:58:34 +0000

    Create 18DifferentIntegerSize.cs

---
+++ 04ProgrammingFundamentals_DataTypesAndVariablesExercise/18DifferentIntegerSize.cs
@@ -0,0 +1,94 @@
+using System;
+
+namespace _18DifferentIntegerSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringNumber = Console.ReadLine();
+            string dataType = string.Empty;
+            bool isFit = false;
+
+            try
+            {
+                sbyte sbyteNumber = sbyte.Parse(stringNumber);
+                dataType += "* sbyte" + Environment.NewLine;
+                isFit = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                byte byteNumber = byte.Parse(stringNumber);
+                dataType += "* byte" + Environment.NewLine;
+                isFit = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                short shortNumber = short.Parse(stringNumber);
+                dataType += "* short" + Environment.NewLine;
+                isFit = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                ushort ushortNumber = ushort.Parse(stringNumber);
+                dataType += "* ushort" + Environment.NewLine;
+                isFit = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                int intNumber = int.Parse(stringNumber);
+                dataType += "* int" + Environment.NewLine;
+                isFit = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                uint uintNumber = uint.Parse(stringNumber);
+                dataType += "* uint" + Environment.NewLine;
+                isFit = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                long longNumber = long.Parse(stringNumber);
+                dataType += "* long";
+                isFit = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            if (isFit)
+            {
+                Console.WriteLine($"{stringNumber} can fit in:");
+                Console.WriteLine(dataType);
+            }
+            else
+            {
+                Console.WriteLine($"{stringNumber} can't fit in any type");
+            }
+        }
+    }
+}
