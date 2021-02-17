8d528f772b8dce0cdfa6c30d5b6b7c8ed784bb68
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 13:51:49 +0000

    Create 15NeighbourWars.cs

---
+++ 02ProgrammingFundamentals_CSharpConditionalStatementsAndLoopsExercise/15NeighbourWars.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace _15NeighbourWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peshoDemage = int.Parse(Console.ReadLine());
+            int goshoDemage = int.Parse(Console.ReadLine());
+            int peshoHealth = 100;
+            int goshoHealth = 100;
+            int turn = 0;
+            while (peshoHealth > 0 && goshoHealth > 0)
+            {
+                turn++;
+                if (turn % 2 != 0)
+                {
+                    goshoHealth -= peshoDemage;
+                    if (goshoHealth <= 0)
+                    {
+                        Console.WriteLine($"Pesho won in {turn}th round.");
+                        break;
+                    }
+
+                    Console.WriteLine($"Pesho used Roundhouse kick and reduced Gosho to {goshoHealth} health.");    
+                }
+                else
+                {
+                    peshoHealth -= goshoDemage;
+                    if (peshoHealth <= 0)
+                    {
+                        Console.WriteLine($"Gosho won in {turn}th round.");
+                        break;
+                    }
+                    Console.WriteLine($"Gosho used Thunderous fist and reduced Pesho to {peshoHealth} health.");     
+                }
+
+                if (turn % 3 == 0)
+                {
+                    peshoHealth += 10;
+                    goshoHealth += 10;
+                }
+            }
+        }
+    }
+}
