ed704450e7ac64a662a7a9f49032149bf31ecce4
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 15:01:25 +0000

    Create 03PracticeCharactersAndStrings.cs

---
+++ 04ProgrammingFundamentals_DataTypesAndVariablesExercise/03PracticeCharactersAndStrings.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace _03PracticeCharactersAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Software University";
+            char ch1 = 'B';
+            char ch2 = 'y';
+            char ch3 = 'e';
+            string str2 = "I love programming";
+            Console.WriteLine(str1);
+            Console.WriteLine(ch1);
+            Console.WriteLine(ch2);
+            Console.WriteLine(ch3);
+            Console.WriteLine(str2);
+        }
+    }
+}
