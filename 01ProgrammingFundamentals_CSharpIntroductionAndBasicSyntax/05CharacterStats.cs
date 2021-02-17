08d6f3148115abc5086e2b7a05884fb3295d0ce9
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 10:21:54 +0000

    Create 05CharacterStats.cs

---
+++ 01ProgrammingFundamentals_CSharpIntroductionAndBasicSyntax/05CharacterStats.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace _05CharacterStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maximumHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maximumEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine("Health: |{0}{1}|", new string('|', currentHealth),
+                new string('.', maximumHealth - currentHealth));
+            Console.WriteLine("Energy: |{0}{1}|", new string('|', currentEnergy),
+                new string('.', maximumEnergy - currentEnergy));
+        }
+    }
+}
