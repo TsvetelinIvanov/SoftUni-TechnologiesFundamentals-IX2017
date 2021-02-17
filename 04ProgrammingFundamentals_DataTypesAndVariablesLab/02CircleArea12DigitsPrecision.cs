598ba296316aedc940d25805f02fb5748ef0df8e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 14:32:04 +0000

    Create 02CircleArea12DigitsPrecision.cs

---
+++ 04ProgrammingFundamentals_DataTypesAndVariablesLab/02CircleArea12DigitsPrecision.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace _02CircleArea12DigitsPrecision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            double area = Math.PI * r * r;
+            Console.WriteLine("{0:f12}", area);
+        }
+    }
+}
