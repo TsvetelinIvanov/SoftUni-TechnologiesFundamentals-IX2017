95c988278e918af4a0542581e92ac4ec45166031
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 12:13:54 +0000

    Create 12NumberChecker.cs

---
+++ 02ProgrammingFundamentals_CSharpConditionalStatementsAndLoopsLab/12NumberChecker.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace _12NumberChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("It is a number.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid input!");
+            }
+        }
+    }
+}
