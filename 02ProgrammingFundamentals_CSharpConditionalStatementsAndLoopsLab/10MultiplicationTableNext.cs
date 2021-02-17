6ebf87fa3ee9be22457f9facfd9e7a8644c6676b
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 12:05:17 +0000

    Create 10MultiplicationTableNext.cs

---
+++ 02ProgrammingFundamentals_CSharpConditionalStatementsAndLoopsLab/10MultiplicationTableNext.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace _10MultiplicationTableNext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int times = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine($"{number} X {times} = {number * times}");
+                times++;
+            } while (times <= 10);
+        }
+    }
+}
