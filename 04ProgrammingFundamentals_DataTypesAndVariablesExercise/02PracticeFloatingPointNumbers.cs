43daf479abebb0aceb6707635d95748b692c208b
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-17 14:58:35 +0000

    Create 02PracticeFloatingPointNumbers.cs

---
+++ 04ProgrammingFundamentals_DataTypesAndVariablesExercise/02PracticeFloatingPointNumbers.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace _02PracticeFloatingPointNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number1 = 3.141592653589793238m;
+            double number2 = 1.60217657;
+            decimal number3 = 7.8184261974584555216535342341m;
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+            Console.WriteLine(number3);
+        }
+    }
+}
