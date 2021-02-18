2328c6a02456ebc33749705c24add65a2a565fe1
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-18 19:45:43 +0000

    Create 08GreaterOfTwoValues.cs

---
+++ 05ProgrammingFundamentals_MethodsLab/08GreaterOfTwoValues.cs
@@ -0,0 +1,76 @@
+using System;
+
+namespace _08GreaterOfTwoValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            switch (type)
+            {
+                case "int":
+                    {
+                        int first = int.Parse(Console.ReadLine());
+                        int second = int.Parse(Console.ReadLine());
+                        int max = GetMax(first, second);
+                        Console.WriteLine(max);
+                        break;
+                    }
+                case "char":
+                    {
+                        char first = char.Parse(Console.ReadLine());
+                        char second = char.Parse(Console.ReadLine());
+                        char max = GetMax(first, second);
+                        Console.WriteLine(max);
+                        break;
+                    }
+                case "string":
+                    {
+                        string first = Console.ReadLine();
+                        string second = Console.ReadLine();
+                        string max = GetMax(first, second);
+                        Console.WriteLine(max);
+                        break;
+                    }
+            }
+        }
+
+        static int GetMax(int first, int second)
+        {
+            if (first >= second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+
+        static char GetMax(char first, char second)
+        {
+            if (first >= second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+
+        static string GetMax(string first, string second)
+        {
+            if (first.CompareTo(second) >= 0)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }        
+    }
+}
