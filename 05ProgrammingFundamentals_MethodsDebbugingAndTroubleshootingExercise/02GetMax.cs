9556cb94c68bfb8d403156bedbc5dce239d7e705
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-20 16:19:13 +0200

    Create 02GetMax.cs

---
+++ 05ProgrammingFundamentals_MethodsDebbugingAndTroubleshootingExercise/02GetMax.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace _02GetMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetMax(GetMax(number1, number2), number3));
+        }
+
+        static int GetMax(int a, int b)
+        {
+            if (a >= b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+    }
+}
