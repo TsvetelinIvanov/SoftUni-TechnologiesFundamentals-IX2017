5b01cf6ea8bf179696b0d44df4477be0a5fb0220
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-20 22:50:36 +0200

    Create 10PairsByDifference.cs

---
+++ 06ProgrammingFundamentals_ArraysExercise/10PairsByDifference.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+
+namespace _10PairsByDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int difference = int.Parse(Console.ReadLine());
+            int counter = 0;            
+
+            for (int i = 0; i <= numbers.Length - 1; i++)
+            {
+                for (int j = 0; j <= numbers.Length - 1; j++)
+                {
+                    if (numbers[i] - numbers[j] == difference)
+                    {
+                        counter++;                        
+                    }
+                }
+            }
+
+            if (difference == 0)
+                counter -= numbers.Length;
+
+            Console.WriteLine(counter);
+        }
+    }
+}
