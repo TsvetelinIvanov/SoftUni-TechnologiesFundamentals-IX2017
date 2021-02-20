c476f52ff9170a5fa5f14bbea4fb148b410cc52a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-20 23:51:12 +0200

    Create 06SquareNumbers.cs

---
+++ 07ProgrammingFundamentals_ListsLab/06SquareNumbers.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06SquareNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            List<int> squareNumbers = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (Math.Sqrt(numbers[i]) == (int)Math.Sqrt(numbers[i]))
+                {
+                    squareNumbers.Add(numbers[i]);
+                }
+            }
+
+            squareNumbers.Sort();
+            squareNumbers.Reverse();
+            Console.WriteLine(string.Join(" ", squareNumbers));
+        }
+    }
+}
