05a51216471a9aa7972313d8d0f568bca619ec0e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-21 01:38:05 +0200

    Create 03SumMinMaxAverageLINQ.cs

---
+++ 08ProgrammingFundamentals_DictionariesLambdaExpressionsAndLINQLab/03SumMinMaxAverageLINQ.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+
+namespace _03SumMinMaxAverageLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Sum = {0}", numbers.Sum());
+            Console.WriteLine("Min = {0}", numbers.Min());
+            Console.WriteLine("Max = {0}", numbers.Max());
+            Console.WriteLine("Average = {0}", numbers.Average());
+        }
+    }
+}
