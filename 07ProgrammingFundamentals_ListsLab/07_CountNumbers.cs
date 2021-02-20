48cf1922504f6778a3fb9ab4ad359596fa06c37b
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-21 00:01:19 +0200

    Create 07_CountNumbers.cs

---
+++ 07ProgrammingFundamentals_ListsLab/07_CountNumbers.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07_CountNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            numbers.Sort();
+            int next = 0;
+            while (next < numbers.Count)
+            {
+                int number = numbers[next], count = 1;
+                while (next + count < numbers.Count && numbers[next + count] == number)
+                {
+                    count++;
+                }
+
+                next += count;
+                Console.WriteLine($"{number} -> {count}");
+            }
+        }
+    }
+}
