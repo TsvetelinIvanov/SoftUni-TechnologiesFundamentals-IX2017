8946cd47e44303c9dbda82423905f78f50038822
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-20 22:42:09 +0200

    Create 08_MostFrequentNumber.cs

---
+++ 06ProgrammingFundamentals_ArraysExercise/08_MostFrequentNumber.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+
+namespace _08_MostFrequentNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int number = 0;
+            int max = 0;
+            int counter = 0;
+            int frequence = 0;
+
+            for (int i = 0; i <= numbers.Length - 1; i++)
+            {
+                counter = 0;
+                for (int j = 0; j <= numbers.Length - 1; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        counter++;
+                        number = numbers[i];
+                        if (frequence < counter)
+                        {
+                            frequence = counter;
+                            max = number;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
