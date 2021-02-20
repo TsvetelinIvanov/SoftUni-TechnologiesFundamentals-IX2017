6b361f37e588ad94e4592acd807ab331e74f9d38
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-20 17:44:16 +0200

    Create 10CubeProperties.cs

---
+++ 05ProgrammingFundamentals_MethodsDebbugingAndTroubleshootingExercise/10CubeProperties.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace _10CubeProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cubeSide = double.Parse(Console.ReadLine());
+            string wishedParameter = Console.ReadLine();
+            double findedParameter = 0.0;
+            switch (wishedParameter)
+            {
+                case "face":
+                    findedParameter = FindeFaceDiagonal(cubeSide);
+                    break;
+                case "space":
+                    findedParameter = FindeSpaceDiagonal(cubeSide);
+                    break;
+                case "area":
+                    findedParameter = FindeSurfaceArea(cubeSide);
+                    break;
+                case "volume":
+                    findedParameter = FindeCubeVolume(cubeSide);
+                    break;
+            }
+
+            Console.WriteLine($"{findedParameter:f2}");
+        }
+
+        static double FindeFaceDiagonal(double side)
+        {
+            double faceDiagonal = Math.Sqrt(2 * side * side);
+            return faceDiagonal;
+        }
+
+        static double FindeSpaceDiagonal(double side)
+        {
+            double spaceDiagonal = Math.Sqrt(3 * side * side);
+            return spaceDiagonal;
+        }
+
+        static double FindeSurfaceArea(double side)
+        {
+            double surfaceArea = 6 * side * side;
+            return surfaceArea;
+        }
+
+        static double FindeCubeVolume(double side)
+        {
+            double cubeVolume = side * side * side;
+            return cubeVolume;
+        }
+    }
+}
