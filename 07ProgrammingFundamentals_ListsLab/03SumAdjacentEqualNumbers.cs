fe4a2f2496f0181695ec786ada6b6f0780671a4d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-20 23:33:20 +0200

    Create 03SumAdjacentEqualNumbers.cs

---
+++ 07ProgrammingFundamentals_ListsLab/03SumAdjacentEqualNumbers.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03SumAdjacentEqualNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .ToList();
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i - 1] == numbers[i])
+                {
+                    numbers[i - 1] += numbers[i];
+                    numbers.RemoveAt(i);
+                    i = i - 2;
+                    if (i < 0)
+                    {
+                        i = 0;
+                    }                        
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
