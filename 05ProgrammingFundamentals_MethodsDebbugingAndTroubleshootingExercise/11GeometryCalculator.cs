a33924d88b7af20d8112eb1335e107fd171f0492
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-20 17:48:22 +0200

    Create 11GeometryCalculator.cs

---
+++ 05ProgrammingFundamentals_MethodsDebbugingAndTroubleshootingExercise/11GeometryCalculator.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace _11GeometryCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine().ToLower();
+            double area = 0.0;
+            switch (figure)
+            {
+                case "triangle":
+                    double side = double.Parse(Console.ReadLine());
+                    double height = double.Parse(Console.ReadLine());
+                    area = GetTriangleArea(side, height);
+                    break;
+                case "rectangle":
+                    double width = double.Parse(Console.ReadLine());
+                    double rectangleHeight = double.Parse(Console.ReadLine());
+                    area = GetRectangleArea(width, rectangleHeight);
+                    break;
+                case "square":
+                    double squareSide = double.Parse(Console.ReadLine());                    
+                    area = GetSquareArea(squareSide);
+                    break;
+                case "circle":
+                    double radius = double.Parse(Console.ReadLine());                    
+                    area = GetCircleArea(radius);
+                    break;
+            }
+
+            Console.WriteLine($"{area:f2}");
+        }
+
+        static double GetTriangleArea(double side, double height)
+        {
+            return side * height / 2;
+        }
+
+        static double GetRectangleArea(double width, double height)
+        {
+            return width * height;
+        }
+
+        static double GetSquareArea(double side)
+        {
+            return side * side;
+        }
+
+        static double GetCircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
