2406d38fad24ea773f75c4d5f79551dceea240d4
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-21 01:25:43 +0200

    Create 01CountRealNumbers.cs

---
+++ 08ProgrammingFundamentals_DictionariesLambdaExpressionsAndLINQLab/01CountRealNumbers.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse).ToArray();
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+            foreach (double number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts[number] = 1;
+                }
+            }
+
+            foreach (double number in counts.Keys)
+            {
+                Console.WriteLine("{0} -> {1}", number, counts[number]);
+            }            
+        }
+    }
+}
