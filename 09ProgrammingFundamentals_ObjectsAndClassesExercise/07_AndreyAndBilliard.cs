78495aabc854f9b99da743c5b751e67950526578
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-21 13:00:55 +0200

    Create 07_AndreyAndBilliard.cs

---
+++ 09ProgrammingFundamentals_ObjectsAndClassesExercise/07_AndreyAndBilliard.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07_AndreyAndBilliard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> clients =
+                new Dictionary<string, Dictionary<string, int>>();
+            Dictionary<string, double> products = new Dictionary<string, double>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] productList = Console.ReadLine().Split('-');
+                if (products.ContainsKey(productList[0]))
+                {
+                    products[productList[0]] = double.Parse(productList[1]);
+                }
+                else
+                {
+                    products.Add(productList[0], double.Parse(productList[1]));
+                }
+            }
+
+            string[] orders = Console.ReadLine().Split('-', ',');
+            while (orders[0] != "end of clients")
+            {
+                string client = orders[0];
+                string product = orders[1];
+                int quantity = int.Parse(orders[2]);
+                if (products.ContainsKey(product) && !clients.ContainsKey(client))
+                {
+                    clients.Add(client, new Dictionary<string, int>());
+                }
+
+                if (products.ContainsKey(product))
+                {
+                    if (!clients[client].ContainsKey(product))
+                    {
+                        clients[client].Add(product, quantity);
+                    }
+                    else
+                    {
+                        clients[client][product] += quantity;
+                    }
+                }
+
+                orders = Console.ReadLine().Split('-', ',');
+            }
+
+            double totalSum = 0;
+            foreach (KeyValuePair<string, Dictionary<string, int>> client in clients.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(client.Key);
+                double sum = 0;
+
+                foreach (KeyValuePair<string, int> product in client.Value)
+                {
+                    Console.WriteLine($"-- {product.Key} - {product.Value}");
+                    sum += product.Value * products[product.Key];
+                }
+
+                Console.WriteLine("Bill: {0:f2}", sum);
+                totalSum += sum;
+            }
+
+            Console.WriteLine("Total bill: {0:f2}", totalSum);
+        }
+    }
+}
