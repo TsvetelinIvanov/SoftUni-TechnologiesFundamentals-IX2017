6d88ece3130cfbd0a2310a233f316de90689de38
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-21 20:56:59 +0200

    Create 01MostFrequentNumber.cs

---
+++ 10ProgrammingFundamentals_FilesAndExceptionsExercise/01MostFrequentNumber.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace _01MostFrequentNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int[] numbers = File.ReadAllText("input.txt")
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int number = 0;
+            int max = 0;
+            int counter = 0;
+            int frequence = 0;
+
+            for (int i = 0; i <= numbers.Length - 1; i++)
+            {
+                counter = 0;
+                for (int j = 0; j <= numbers.Length - 1; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        counter++;
+                        number = numbers[i];
+                        if (frequence < counter)
+                        {
+                            frequence = counter;
+                            max = number;
+                        }
+                    }
+                }
+            }
+            
+            File.WriteAllText("output.txt", max.ToString());
+        }
+    }
+}
