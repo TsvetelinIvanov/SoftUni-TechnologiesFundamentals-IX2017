af6c4169b42708ddff15a7dd319cfef51a5910d3
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-21 21:03:37 +0200

    Create 02IndexOfLetters.cs

---
+++ 10ProgrammingFundamentals_FilesAndExceptionsExercise/02IndexOfLetters.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+
+namespace _02IndexOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = File.ReadAllText("input.txt").ToLower();
+            char[] alphabet = new char[26];
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                alphabet[i - 'a'] = i;
+            }
+
+            string text = String.Empty;
+            for (int i = 0; i <= word.Length - 1; i++)
+            {
+                for (int j = 0; j <= alphabet.Length - 1; j++)
+                {
+                    if (word[i] == alphabet[j])
+                    {
+                        text += $"{word[i]} -> {j}\r\n";
+                    }
+                }
+            }
+
+            File.WriteAllText("output.txt", text);
+        }
+    }
+}
