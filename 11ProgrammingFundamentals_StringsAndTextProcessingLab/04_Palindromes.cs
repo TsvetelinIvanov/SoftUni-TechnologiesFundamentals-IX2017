71e7fc0a03d69991d869760ea4f7a3ec872daa05
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-21 22:48:01 +0200

    Create 04_Palindromes.cs

---
+++ 11ProgrammingFundamentals_StringsAndTextProcessingLab/04_Palindromes.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(new char[] { ',', ' ', '?', '!', '.' },
+                StringSplitOptions.RemoveEmptyEntries).ToArray();
+            List<string> palindromes = new List<string>();
+            foreach (string word in words)
+            {
+                if (word.SequenceEqual(word.Reverse()))
+                {
+                    palindromes.Add(word);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", palindromes.OrderBy(p => p).Distinct()));
+        }
+    }
+}
