8155462a13ce57ae3e4a1dee5b92361d345f2248
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-21 11:21:15 +0200

    Create 03BigFactorial.cs

---
+++ 09ProgrammingFundamentals_ObjectsAndClassesLab/03BigFactorial.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Numerics;
+
+namespace _03BigFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
