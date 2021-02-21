be24afd5679d313b4f66ab7707dd14aa10030e0c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-21 11:18:38 +0200

    Create 02RandomizeWords.cs

---
+++ 09ProgrammingFundamentals_ObjectsAndClassesLab/02RandomizeWords.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace _02RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Random random = new Random();
+            for (int i = 0; i < words.Length; i++)
+            {
+                int newI = random.Next(words.Length);
+                string word = words[i];
+                words[i] = words[newI];
+                words[newI] = word;
+            }
+
+            Console.WriteLine(string.Join($"{Environment.NewLine}", words));
+        }
+    }
+}
