efeb746803c32c11e426aaf2933693c225138a82
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-22 23:50:00 +0200

    Create 03MatchHexadecimalNumbers.cs

---
+++ 12ProgrammingFundamentals_RegularExpressionsLab/03MatchHexadecimalNumbers.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03MatchHexadecimalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b0?x?[0-9A-F]+\b";
+            string name = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(name, pattern);
+            string[] result = matches.Cast<Match>().Select(x => x.Value).ToArray();
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
