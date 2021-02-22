ae1b8addcb5f4099a71b19b6e9c16c82a5fbc2a5
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-22 23:59:45 +0200

    Create 05MatchNumbers.cs

---
+++ 12ProgrammingFundamentals_RegularExpressionsLab/05MatchNumbers.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace _05MatchNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(^|(?<=\s))-?(\d+|\d+\.\d+)($|(?=\s))";
+            string name = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(name, pattern);
+            foreach (Match match in matches)
+            {
+                Console.Write(match.Value + " ");
+            }
+        }
+    }
+}
