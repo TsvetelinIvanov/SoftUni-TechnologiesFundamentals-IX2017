d89fc6a8dc99290e81e45e6048025cbba08d0f6c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-22 23:48:00 +0200

    Create 02MatchPhoneNumber.cs

---
+++ 12ProgrammingFundamentals_RegularExpressionsLab/02MatchPhoneNumber.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02MatchPhoneNumber
+{
+    class MatchPhoneNumber
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"(^| )\+359(-| )2\2\d{3}\2\d{4}\b");
+            string name = Console.ReadLine();
+            MatchCollection matches = regex.Matches(name);
+            string[] result = matches.Cast<Match>().Select(x => x.Value.Trim()).ToArray();
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
