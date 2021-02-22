79212d07ec7021f3fc74edbd5da6876c87342b42
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-23 00:10:39 +0200

    Create 06ReplaceATag.cs

---
+++ 12ProgrammingFundamentals_RegularExpressionsLab/06ReplaceATag.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace _06ReplaceATag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"<a.*?href=(.*?)>(.*?)<\/a>";
+            string replacement = @"[URL href=$1]$2[/URL]";
+            string text = Console.ReadLine();
+            while (text != "end")
+            {
+                string result = Regex.Replace(text, pattern, replacement);
+                Console.WriteLine(result);
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
