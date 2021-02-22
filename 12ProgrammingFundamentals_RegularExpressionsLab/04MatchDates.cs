e02774c42bf8aaf925a7299a7ed3c28d9da3a824
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-22 23:54:52 +0200

    Create 04MatchDates.cs

---
+++ 12ProgrammingFundamentals_RegularExpressionsLab/04MatchDates.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace _04MatchDates
+{
+    class MatchDates
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>\d{2})(\.|-|\/)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+            string name = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(name, pattern);
+            foreach (Match match in matches)
+            {
+                Console.WriteLine("Day: {0}, Month: {1}, Year: {2}",
+                    match.Groups["day"], match.Groups["month"], match.Groups["year"]);
+            }
+        }
+    }
+}
