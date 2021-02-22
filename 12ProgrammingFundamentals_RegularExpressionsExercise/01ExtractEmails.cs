2897b5c4d7d54b7eaa7d73f2bedd39230105b706
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-23 00:17:37 +0200

    Create 01ExtractEmails.cs

---
+++ 12ProgrammingFundamentals_RegularExpressionsExercise/01ExtractEmails.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace ExtractEmailsI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string pattern = @"(^|(?<=\s))[A-Za-z0-9][\w\.-]*[A-Za-z0-9]@[A-Za-z][A-Za-z\.-]*[A-Za-z]\.[A-Za-z][A-Za-z\.-]*[A-Za-z]($|(?=[., \n]))";
+            string pattern = @"(^|(?<=\s))[A-Za-z0-9][\w\.-]*[A-Za-z0-9]@[A-Za-z][A-Za-z\.-]*[A-Za-z]\.[A-Za-z][A-Za-z\.-]*[A-Za-z]";
+            string text = Console.ReadLine();
+            MatchCollection emails = Regex.Matches(text, pattern);
+            foreach (Match email in emails)
+            {
+                Console.WriteLine(email);
+            }
+        }
+    }
+}
