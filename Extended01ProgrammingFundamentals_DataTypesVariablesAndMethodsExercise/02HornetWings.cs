fdb206257b0e487164eac3cca4a27f86ea4a2113
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-23 06:25:46 +0200

    Create 02HornetWings.cs

---
+++ Extended01ProgrammingFundamentals_DataTypesVariablesAndMethodsExercise/02HornetWings.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace _02HornetWings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wingFlaps = int.Parse(Console.ReadLine());
+            double distanceFor1000WingFlaps = double.Parse(Console.ReadLine());
+            int endurance = int.Parse(Console.ReadLine());
+
+            double distance = (wingFlaps / 1000) * distanceFor1000WingFlaps;
+            long seconds = (wingFlaps / 100) + ((wingFlaps / endurance) * 5);
+
+            Console.WriteLine("{0:f2} m.", distance);
+            Console.WriteLine(seconds + " s.");
+        }
+    }
+}
