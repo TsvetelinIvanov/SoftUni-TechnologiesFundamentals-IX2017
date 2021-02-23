da062770f6a552a330626c11cd7d7eb14b78c7e2
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-23 05:35:31 +0200

    Create 06ValidUsernames.cs

---
+++ 12ProgrammingFundamentals_RegularExpressionsExercise/06ValidUsernames.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _06ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                .Split(new char[] { ' ', '/', '\\', ')', '(' }, StringSplitOptions.RemoveEmptyEntries);
+            string pattern = @"^[A-Za-z]\w{2,24}$";
+            List<string> validUsernames = new List<string>();
+            foreach (string username in usernames)
+            {
+                if (Regex.IsMatch(username, pattern))
+                {
+                    validUsernames.Add(username);
+                }
+            }
+
+            string first = string.Empty;
+            string second = string.Empty;
+            int length = 0;
+            for (int i = 0; i < validUsernames.Count - 1; i++)
+            {
+                if (length < validUsernames[i].Length + validUsernames[i + 1].Length)
+                {
+                    length = validUsernames[i].Length + validUsernames[i + 1].Length;
+                    first = validUsernames[i];
+                    second = validUsernames[i + 1];
+                }
+            }
+
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+        }
+    }
+}
