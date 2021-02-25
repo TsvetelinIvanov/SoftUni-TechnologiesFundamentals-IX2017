2bde6af945dbd83df68c46fcc6a198727a884b5c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-25 10:21:45 +0200

    Create 04HornetAssault.cs

---
+++ Extended02ProgrammingFundamentals_ArraysListsArrayAndListAlgorithmsExercise/04HornetAssault.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04HornetAssault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] beehives = Console.ReadLine()
+                 .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                 .Select(long.Parse).ToArray();
+            List<long> hornets = Console.ReadLine()
+                  .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                  .Select(long.Parse).ToList();
+
+            for (int i = 0; i < beehives.Length; i++)
+            {
+                long hornetsPower = hornets.Sum();
+                if (hornetsPower <= beehives[i])
+                {
+                    beehives[i] -= hornetsPower;
+                    hornets.RemoveAt(0);
+                }
+                else
+                {
+                    beehives[i] = 0;
+                }
+
+                if (hornets.Sum() == 0)
+                    break;
+            }
+
+            if (beehives.Sum() > 0)
+                Console.WriteLine(string.Join(" ", beehives.Where(x => x > 0)));
+            else
+                Console.WriteLine(string.Join(" ", hornets.Where(x => x > 0)));
+        }
+    }
+}
