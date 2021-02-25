1e253cba6dde2ac43b5a782dcb13e16cbe1f0985
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-25 11:04:54 +0200

    Create 08MatchDates.cs

---
+++ Extended03ProgrammingFundamentals_StringsAndRegularExpressionsLab/08MatchDates.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace _08MatchDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                             
+            string pattern = @"\b(?<day>\d{2})([-\.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+            string datesString = Console.ReadLine();
+            MatchCollection dates = Regex.Matches(datesString, pattern);
+            foreach (Match date in dates)
+            {                
+                Console.WriteLine($"Day: {date.Groups["day"]}, Month: {date.Groups["month"]}, " +
+                $"Year: {date.Groups["year"]}");
+                //Console.WriteLine($"Day: {date.Groups["day"].Value}, Month: {date.Groups["month"].Value}, " +
+                // $"Year: {date.Groups["year"].Value}");
+                //Console.WriteLine("Day: {0}, Month: {1}, Year: {2}", 
+                //date.Groups["day"].Value, date.Groups["month"].Value, date.Groups["year"].Value); 
+                //string day = date.Groups["day"].Value;
+                //string month = date.Groups["month"].Value;
+                //string year = date.Groups["year"].Value;
+                //Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
