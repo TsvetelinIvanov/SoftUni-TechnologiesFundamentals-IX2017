c09082f0aea2dd25af439eec9c8bae7fa14685ee
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-25 09:45:24 +0200

    Create 03PokemonDoNotGo.cs

---
+++ Extended02ProgrammingFundamentals_ArraysListsArrayAndListAlgorithmsExercise/03PokemonDoNotGo.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03PokemonDoNotGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> pokemons = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+            long sum = 0;
+            while (pokemons.Count > 0)
+            {
+                int index = int.Parse(Console.ReadLine());
+                int value = 0;
+                if (index < 0)
+                {
+                    value = pokemons[0];
+                    sum += pokemons[0];
+                    pokemons[0] = pokemons[pokemons.Count - 1];
+                }
+                else if (index >= pokemons.Count)
+                {
+                    value = pokemons[pokemons.Count - 1];
+                    sum += pokemons[pokemons.Count - 1];
+                    pokemons[pokemons.Count - 1] = pokemons[0];
+                }
+                else
+                {
+                    value = pokemons[index];
+                    sum += pokemons[index];
+                    pokemons.RemoveAt(index);
+                }
+
+                IncreaseOrDecrease(pokemons, value);
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static void IncreaseOrDecrease(List<int>numbers, int value)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] <= value)
+                    numbers[i] += value;
+                else
+                    numbers[i] -= value;
+            }
+        }
+    }
+}
