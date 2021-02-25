4ce729e14fa5cbb4d39fc8706b32a77b79ee25ef
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-25 12:14:23 +0200

    Create 05Largest3Numbers.cs

---
+++ Extended04ProgrammingFundamentals_DictionariesLINQandLambdaExpressionsLab/05Largest3Numbers.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+
+namespace _05Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse).ToArray();
+            numbers = numbers.OrderByDescending(x => x).Take(3).ToArray();
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
