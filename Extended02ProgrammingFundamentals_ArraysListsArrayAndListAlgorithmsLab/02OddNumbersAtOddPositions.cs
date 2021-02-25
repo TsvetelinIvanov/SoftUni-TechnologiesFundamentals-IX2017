9031a35426d29258e0ca59758155e1f16e662528
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-25 08:46:06 +0200

    Create 02OddNumbersAtOddPositions.cs

---
+++ Extended02ProgrammingFundamentals_ArraysListsArrayAndListAlgorithmsLab/02OddNumbersAtOddPositions.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+
+namespace _02OddNumbersAtOddPositions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            for (int i = 1; i <= numbers.Length - 1; i += 2)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    Console.WriteLine("Index {0} -> {1}", i, numbers[i]);
+                }
+            }
+
+            //or - it can be used for oder steps:
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (i % 2 == 1 && Math.Abs(numbers[i] % 2) == 1)
+            //        Console.WriteLine("Index {0} -> {1}", i, numbers[i]);
+            //}
+        }
+    }
+}
