491f56c0f72f63a10fdc489afa4c825b951d7a5c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-25 08:55:14 +0200

    Create 05LargestNElements.cs

---
+++ Extended02ProgrammingFundamentals_ArraysListsArrayAndListAlgorithmsLab/05LargestNElements.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05LargestNElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            int n = int.Parse(Console.ReadLine());
+            numbers.Sort();
+            numbers.Reverse();
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            //or 
+            //List<int> largestNElements = new List<int>();
+            //for (int i = 0; i < n; i++)
+            //{
+            //    largestNElements.Add(numbers[i]);
+            //}
+
+            //Console.WriteLine(string.Join(" ", largestNElements));
+        }
+    }
+}
