307dfd0d45a7fb5eb3fc8904dc334d6bb7eeaf24
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-25 10:52:55 +0200

    Create 05MatchFullName.cs

---
+++ Extended03ProgrammingFundamentals_StringsAndRegularExpressionsLab/05MatchFullName.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace _05MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            string names = Console.ReadLine();
+            MatchCollection matchedNames = Regex.Matches(names, pattern);
+            foreach (Match name in matchedNames)
+            {
+                Console.Write(name + " ");
+                //Console.Write(name.Value + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
