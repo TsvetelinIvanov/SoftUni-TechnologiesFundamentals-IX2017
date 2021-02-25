049f4a1dce6aef53014140472b67cfa34bc1083f
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-25 17:54:03 +0200

    Create 03RageQuit.cs

---
+++ 15ProgrammingFundamentals_ExamPreparationIII/03RageQuit.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _03RageQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([^0-9]+)(\d+)";
+            string input = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+            foreach (Match match in Regex.Matches(input, pattern))
+            {
+                string word = match.Groups[1].Value.ToUpper();
+                int count = int.Parse(match.Groups[2].Value);                
+                for (int i = 0; i < count; i++)
+                {
+                    result.Append(word);
+                }
+            }
+
+            int uniqueSumbolsCount = result.ToString().Distinct().Count();
+            Console.WriteLine("Unique symbols used: " + uniqueSumbolsCount);
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
