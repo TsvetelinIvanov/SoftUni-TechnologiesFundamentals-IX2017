d0f1a8b93e99b659672304af4f3e0068a03b6b2e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-02 22:23:25 +0200

    Create 03Regexmon.cs

---
+++ ExamProgrammingFundamentals9VII2017/03Regexmon.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace _03Regexmon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bojoPattern = @"([A-Za-z]+\-[A-Za-z]+)";
+            string didiPattern = @"([^A-Za-z-]+)";
+            string input = Console.ReadLine();
+            while (true)
+            {
+                Match didiMatch = Regex.Match(input, didiPattern);
+                if (didiMatch.Success)
+                {
+                    Console.WriteLine(didiMatch.Value);
+                    input = input.Substring(input.IndexOf(didiMatch.Value) + didiMatch.Value.Length);
+                }
+                else
+                {
+                    break;
+                }
+
+                Match bojoMatch = Regex.Match(input, bojoPattern);
+                if (bojoMatch.Success)
+                {
+                    Console.WriteLine(bojoMatch.Value);
+                    input = input.Substring(input.IndexOf(bojoMatch.Value) + bojoMatch.Value.Length);
+                }
+                else
+                {
+                    break;
+                }
+
+                //if we wish to remove the match and remain the remainder before the match:
+                //Match didiMatch = Regex.Match(input, didiPattern);
+                //if (didiMatch.Success)
+                //{
+                //    Console.WriteLine(didiMatch.Value);
+                //    input = input.Remove(input.IndexOf(didiMatch.Value), didiMatch.Value.Length);                 
+                //}
+                //else
+                //{
+                //    break;
+                //}
+
+                //Match bojoMatch = Regex.Match(input, bojoPattern);
+                //if (bojoMatch.Success)
+                //{
+                //    Console.WriteLine(bojoMatch.Value);
+                //    input = input.Remove(input.IndexOf(bojoMatch.Value), bojoMatch.Value.Length);
+                //}
+                //else
+                //{
+                //    break;
+                //}
+            }
+        }
+    }
+}
