c8ca33f9a5dfc4ba8f0c208230aa11e8dc85f71b
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-03 06:15:55 +0200

    Create 01Raindrops.cs

---
+++ ExtendedExamProgrammingFundamentals10XII2017/01Raindrops.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+
+namespace _01Raindrops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int regionsAmount = int.Parse(Console.ReadLine());
+            decimal density = decimal.Parse(Console.ReadLine());
+            decimal regionalCoefficient = 0;
+            decimal regionalCoefficientSum = 0;
+
+            for (int i = 0; i < regionsAmount; i++)
+            {
+                decimal[] regionData = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(decimal.Parse).ToArray();
+                decimal raindropsCount = regionData[0];
+                decimal squareMeters = regionData[1];
+
+                regionalCoefficient = raindropsCount / squareMeters;
+                regionalCoefficientSum += regionalCoefficient;
+            }
+            if (density != 0)
+            {
+                decimal result = regionalCoefficientSum / density;
+                Console.WriteLine($"{result:f3}");
+            }
+            else
+            {
+                Console.WriteLine($"{regionalCoefficientSum:f3}");
+            }
+        }
+    }
+}
