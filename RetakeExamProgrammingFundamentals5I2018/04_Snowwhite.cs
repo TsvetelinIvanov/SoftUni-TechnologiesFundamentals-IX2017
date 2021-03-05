c27d5447e08004cabe645417c548116266c546c3
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-05 07:28:47 +0200

    Create 04_Snowwhite.cs

---
+++ RetakeExamProgrammingFundamentals5I2018/04_Snowwhite.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04Snowwhite
+{
+    class Dwarf
+    {
+        public string Name { get; set; }
+        public string HatColor { get; set; }
+        public int Physics { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dwarf> dwarfs = new List<Dwarf>();
+            string input = Console.ReadLine();
+            while (input != "Once upon a time")
+            {
+                string[] dwarfsData = input.Split(new string[] { " <:> " }, StringSplitOptions.None);
+                string dwarfName = dwarfsData[0];
+                string dwarfHatColor = dwarfsData[1];
+                int dwarfPhysics = int.Parse(dwarfsData[2]);                
+                
+                if (!dwarfs.Any(d => d.Name == dwarfName && d.HatColor == dwarfHatColor))
+                {
+                    Dwarf dwarf = new Dwarf
+                    {
+                        Name = dwarfName,
+                        HatColor = dwarfHatColor,
+                        Physics = dwarfPhysics
+                    };
+
+                    dwarfs.Add(dwarf);
+                }
+                else 
+                {
+                    Dwarf dwarf = dwarfs.Single(d => d.Name == dwarfName && d.HatColor == dwarfHatColor);
+                    if (dwarf.Physics < dwarfPhysics)
+                    {
+                        dwarf.Physics = dwarfPhysics;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+           
+            foreach (Dwarf dwarf in dwarfs.OrderByDescending(d => d.Physics)
+                .ThenByDescending(d => dwarfs.Where(dw => dw.HatColor == d.HatColor).Count()))
+            {
+               Console.WriteLine($"({dwarf.HatColor}) {dwarf.Name} <-> {dwarf.Physics}");
+            }
+        }
+    }
+}
