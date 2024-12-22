e06131adaf3326317d76ec9b2004f1f016433142
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-22 10:52:28 +0200

    Update 12TestNumbers.cs

--- 02ProgrammingFundamentals_CSharpConditionalStatementsAndLoopsExercise/12TestNumbers.cs
+++ 02ProgrammingFundamentals_CSharpConditionalStatementsAndLoopsExercise/12TestNumbers.cs
@@ -9,6 +9,7 @@ namespace _12TestNumbers
             int N = int.Parse(Console.ReadLine());
             int M = int.Parse(Console.ReadLine());
             int boundarySum = int.Parse(Console.ReadLine());
+            
             int counter = 0;
             int sum = 0;
             for (int i = N; i >= 1; i--)
@@ -18,11 +19,17 @@ namespace _12TestNumbers
                     counter++;
                     sum += 3 * (i * j);
                     if (sum >= boundarySum)
+                    {
                         break;
+                    }
                 }
+                
                 if (sum >= boundarySum)
+                {
                     break;
+                }
             }
+            
             if (sum >= boundarySum)
             {
                 Console.WriteLine("{0} combinations", counter);
