15cf1f7965d597850376e7e81553d9f632d64b4c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-22 10:22:01 +0200

    Update 03RestaurantDiscount.cs

--- 02ProgrammingFundamentals_CSharpConditionalStatementsAndLoopsExercise/03RestaurantDiscount.cs
+++ 02ProgrammingFundamentals_CSharpConditionalStatementsAndLoopsExercise/03RestaurantDiscount.cs
@@ -8,8 +8,9 @@ namespace _03RestaurantDiscount
         {
             int groupSize = int.Parse(Console.ReadLine());
             string package = Console.ReadLine();
-            double priceRoom = 0.0;
-            double pricePackage = 0.0;
+            
+            double roomPrice = 0.0;
+            double packagePrice = 0.0;
             double discount = 0.0;
             double price = 0.0;
             double pricePerPerson = 0.0;
@@ -19,17 +20,17 @@ namespace _03RestaurantDiscount
             if (groupSize > 0 && groupSize <= 50)
             {
                 room = "Small Hall";
-                priceRoom = 2500.0;
+                roomPrice = 2500.0;
             }
             else if (groupSize > 50 && groupSize <= 100)
             {
                 room = "Terrace";
-                priceRoom = 5000.0;
+                roomPrice = 5000.0;
             }
             else if (groupSize > 100 && groupSize <= 120)
             {
                 room = "Great Hall";
-                priceRoom = 7500.0;
+                roomPrice = 7500.0;
             }
             else if (groupSize > 120)
             {
@@ -39,22 +40,21 @@ namespace _03RestaurantDiscount
             switch (package)
             {
                 case "Normal":
-                    pricePackage = 500;
-                    discount = (priceRoom + pricePackage) * 0.05;
+                    packagePrice = 500;
+                    discount = (roomPrice + packagePrice) * 0.05;
                     break;
                 case "Gold":
-                    pricePackage = 750;
-                    discount = (priceRoom + pricePackage) * 0.1;
+                    packagePrice = 750;
+                    discount = (roomPrice + packagePrice) * 0.1;
                     break;
                 case "Platinum":
-                    pricePackage = 1000;
-                    discount = (priceRoom + pricePackage) * 0.15;
+                    packagePrice = 1000;
+                    discount = (roomPrice + packagePrice) * 0.15;
                     break;
             }
 
-            price = (priceRoom + pricePackage) - discount;
+            price = (roomPrice + packagePrice) - discount;
             pricePerPerson = price / groupSize;
-
             if (isGroupSize == false)
             {
                 Console.WriteLine("We do not have an appropriate hall.");
@@ -64,7 +64,6 @@ namespace _03RestaurantDiscount
                 Console.WriteLine($"We can offer you the {room}");
                 Console.WriteLine($"The price per person is {pricePerPerson:f2}$");
             }
-
         }
     }
 }
