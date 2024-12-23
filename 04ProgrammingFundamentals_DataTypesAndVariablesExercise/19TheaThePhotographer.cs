3463e014d95023d61ad5f8aa4ea112d00f04297f
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-23 20:11:28 +0200

    Update 19TheaThePhotographer.cs

--- 04ProgrammingFundamentals_DataTypesAndVariablesExercise/19TheaThePhotographer.cs
+++ 04ProgrammingFundamentals_DataTypesAndVariablesExercise/19TheaThePhotographer.cs
@@ -6,13 +6,13 @@ namespace _19TheaThePhotographer
     {
         static void Main(string[] args)
         {
-            int takedPictures = int.Parse(Console.ReadLine());
+            int takedPicturesCount = int.Parse(Console.ReadLine());
             int filterTime = int.Parse(Console.ReadLine());
-            int percentFilteredPictures = int.Parse(Console.ReadLine());
+            int filteredPicturesPercent = int.Parse(Console.ReadLine());
             int uploadingTime = int.Parse(Console.ReadLine());
 
-            int filteredPictures = (int)(Math.Ceiling(takedPictures * (percentFilteredPictures / 100.0)));
-            long totalTimeInSeconds = takedPictures * (long)filterTime + filteredPictures * (long)uploadingTime;       
+            int filteredPicturesCount = (int)(Math.Ceiling(takedPicturesCount * (filteredPicturesPercent / 100.0)));
+            long totalTimeInSeconds = takedPicturesCount * (long)filterTime + filteredPicturesCount * (long)uploadingTime;       
 
             long totalMinutes = totalTimeInSeconds / 60;
             long seconds = totalTimeInSeconds % 60;
@@ -22,7 +22,6 @@ namespace _19TheaThePhotographer
             long days = totalHours / 24;
 
             Console.WriteLine($"{days}:{hours:d2}:{minutes:d2}:{seconds:d2}");
-
             //or with TimeSpan:
             //Console.WriteLine(TimeSpan.FromSeconds(totalTimeInSeconds).ToString(new string('d', 1) + @"\:hh\:mm\:ss"));
         }
