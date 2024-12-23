2e13f9c9b295b401bc9813cf06f06533133351fe
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-23 19:55:08 +0200

    Update 16ComparingFloats.cs

--- 04ProgrammingFundamentals_DataTypesAndVariablesExercise/16ComparingFloats.cs
+++ 04ProgrammingFundamentals_DataTypesAndVariablesExercise/16ComparingFloats.cs
@@ -8,8 +8,9 @@ namespace _16ComparingFloats
         {
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
-            bool equal = (Math.Abs(a - b) < 0.000001);
-            Console.WriteLine(equal);
+            
+            bool areEqual = (Math.Abs(a - b) < 0.000001);
+            Console.WriteLine(areEqual);
         }
     }
 }
