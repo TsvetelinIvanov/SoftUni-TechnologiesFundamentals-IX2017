8d1ffad2b8df120ee6bb2392e08a4207c3096919
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-24 16:20:47 +0200

    Update 08RefactorVolumeOfPyramid.cs

--- 04ProgrammingFundamentals_DataTypesAndVariablesLab/08RefactorVolumeOfPyramid.cs
+++ 04ProgrammingFundamentals_DataTypesAndVariablesLab/08RefactorVolumeOfPyramid.cs
@@ -8,10 +8,13 @@ namespace _08RefactorVolumeOfPyramid
         {
             Console.Write("Length: ");
             double length = double.Parse(Console.ReadLine());
+            
             Console.Write("Width: ");
             double width = double.Parse(Console.ReadLine());
+            
             Console.Write("Height: ");
             double height = double.Parse(Console.ReadLine());
+            
             double pyramidVolume = (length * width * height) / 3;
             Console.WriteLine("Pyramid Volume: {0:f2}", pyramidVolume);
         }
