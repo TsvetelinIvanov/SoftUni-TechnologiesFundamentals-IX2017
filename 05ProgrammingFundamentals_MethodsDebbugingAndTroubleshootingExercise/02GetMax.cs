9f08657f0346e983ad03682eefd3f3bfe2585ec7
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-28 14:52:46 +0200

    Update 02GetMax.cs

--- 05ProgrammingFundamentals_MethodsDebbugingAndTroubleshootingExercise/02GetMax.cs
+++ 05ProgrammingFundamentals_MethodsDebbugingAndTroubleshootingExercise/02GetMax.cs
@@ -9,6 +9,7 @@ namespace _02GetMax
             int number1 = int.Parse(Console.ReadLine());
             int number2 = int.Parse(Console.ReadLine());
             int number3 = int.Parse(Console.ReadLine());
+            
             Console.WriteLine(GetMax(GetMax(number1, number2), number3));
         }
 
