3578efa4d2f43db28734d9760720b38ba81403da
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-30 10:13:34 +0200

    Update 16InstructionSetDebugging.cs

--- 05ProgrammingFundamentals_MethodsDebbugingAndTroubleshootingExercise/16InstructionSetDebugging.cs
+++ 05ProgrammingFundamentals_MethodsDebbugingAndTroubleshootingExercise/16InstructionSetDebugging.cs
@@ -6,46 +6,43 @@ namespace _16InstructionSetDebugging
     {
         static void Main(string[] args)
         {
-            string opCode = Console.ReadLine();
-
-            while (opCode != "END")
+            string operationCode = Console.ReadLine();
+            while (operationCode != "END")
             {
-
-                string[] codeArgs = opCode.Split(' ');
+                string[] codeArguments = operationCode.Split(' ');
                 long result = 0;
-
-                switch (codeArgs[0])
+                switch (codeArguments[0])
                 {
                     case "INC":
-                        {
-                            long operandOne = long.Parse(codeArgs[1]);
-                            result = operandOne + 1;
-                            break;
-                        }
+                    {
+                        long operandOne = long.Parse(codeArguments[1]);
+                        result = operandOne + 1;
+                        break;
+                    }
                     case "DEC":
-                        {
-                            long operandOne = long.Parse(codeArgs[1]);
-                            result = operandOne - 1;
-                            break;
-                        }
+                    {
+                        long operandOne = long.Parse(codeArguments[1]);
+                        result = operandOne - 1;
+                        break;
+                    }
                     case "ADD":
-                        {
-                            long operandOne = long.Parse(codeArgs[1]);
-                            long operandTwo = long.Parse(codeArgs[2]);
-                            result = operandOne + operandTwo;
-                            break;
-                        }
+                    {
+                        long operandOne = long.Parse(codeArguments[1]);
+                        long operandTwo = long.Parse(codeArguments[2]);
+                        result = operandOne + operandTwo;
+                        break;
+                    }
                     case "MLA":
-                        {
-                            long operandOne = long.Parse(codeArgs[1]);
-                            long operandTwo = long.Parse(codeArgs[2]);
-                            result = operandOne * operandTwo;
-                            break;
-                        }
+                    {
+                        long operandOne = long.Parse(codeArguments[1]);
+                        long operandTwo = long.Parse(codeArguments[2]);
+                        result = operandOne * operandTwo;
+                        break;
+                    }
                 }
                 
                 Console.WriteLine(result);
-                opCode = Console.ReadLine();
+                operationCode = Console.ReadLine();
             }
         }
     }
