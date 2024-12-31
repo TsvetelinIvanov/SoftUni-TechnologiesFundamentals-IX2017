b62cee85e21e458f534e808e03f8e9e425415aaf
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2024-12-31 19:50:47 +0200

    Update 08GreaterOfTwoValues.cs

--- 05ProgrammingFundamentals_MethodsLab/08GreaterOfTwoValues.cs
+++ 05ProgrammingFundamentals_MethodsLab/08GreaterOfTwoValues.cs
@@ -7,33 +7,32 @@ namespace _08GreaterOfTwoValues
         static void Main(string[] args)
         {
             string type = Console.ReadLine();
-
             switch (type)
             {
                 case "int":
-                    {
-                        int first = int.Parse(Console.ReadLine());
-                        int second = int.Parse(Console.ReadLine());
-                        int max = GetMax(first, second);
-                        Console.WriteLine(max);
-                        break;
-                    }
+                {
+                    int first = int.Parse(Console.ReadLine());
+                    int second = int.Parse(Console.ReadLine());
+                    int max = GetMax(first, second);
+                    Console.WriteLine(max);
+                    break;
+                }
                 case "char":
-                    {
-                        char first = char.Parse(Console.ReadLine());
-                        char second = char.Parse(Console.ReadLine());
-                        char max = GetMax(first, second);
-                        Console.WriteLine(max);
-                        break;
-                    }
+                {
+                    char first = char.Parse(Console.ReadLine());
+                    char second = char.Parse(Console.ReadLine());
+                    char max = GetMax(first, second);
+                    Console.WriteLine(max);
+                    break;
+                }
                 case "string":
-                    {
-                        string first = Console.ReadLine();
-                        string second = Console.ReadLine();
-                        string max = GetMax(first, second);
-                        Console.WriteLine(max);
-                        break;
-                    }
+                {
+                    string first = Console.ReadLine();
+                    string second = Console.ReadLine();
+                    string max = GetMax(first, second);
+                    Console.WriteLine(max);
+                    break;
+                }
             }
         }
 
