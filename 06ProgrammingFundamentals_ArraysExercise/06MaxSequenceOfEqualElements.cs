263bfaefbf25da3709db3ee61c38d6aaa488b8c6
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-01-02 19:14:09 +0200

    Update 06MaxSequenceOfEqualElements.cs

--- 06ProgrammingFundamentals_ArraysExercise/06MaxSequenceOfEqualElements.cs
+++ 06ProgrammingFundamentals_ArraysExercise/06MaxSequenceOfEqualElements.cs
@@ -7,32 +7,31 @@ namespace _06MaxSequenceOfEqualElements
     {
         static void Main(string[] args)
         {
-            int[] sequence = Console.ReadLine()
-                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToArray();
+            int[] sequence = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            
             int start = 0;
-            int lenght = 0;
-            int longestLenght = 0;
+            int length = 0;
+            int longestLength = 0;
 
             for (int i = 0; i < sequence.Length - 1; i++)
             {
                 if (sequence[i] == sequence[i + 1])
                 {
-                    lenght++;
-                    if (lenght > longestLenght)
+                    length++;
+                    if (length > longestLength)
                     {
-                        start = i - lenght;
-                        longestLenght = lenght;
+                        start = i - length;
+                        longestLength = length;
                     }
                 }
                 else
                 {
-                    lenght = 0;
+                    length = 0;
                 }
             }
 
-            for (int i = start + 1; i <= start + longestLenght + 1; i++)
+            for (int i = start + 1; i <= start + longestLength + 1; i++)
             {
                 Console.Write(sequence[i] + " ");
             }
