2d97b1d5996f90f4673645fb15e73e4852c0b0ae
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-01-04 20:11:39 +0200

    Update 04TripleSum.cs

--- 06ProgrammingFundamentals_ArraysLab/04TripleSum.cs
+++ 06ProgrammingFundamentals_ArraysLab/04TripleSum.cs
@@ -7,8 +7,8 @@ namespace _04TripleSum
     {
         static void Main(string[] args)
         {
-            string values = Console.ReadLine();
-            int[] numbers = values.Split(' ').Select(int.Parse).ToArray();
+            string numbersString = Console.ReadLine();
+            int[] numbers = numbersString.Split(' ').Select(int.Parse).ToArray();
             
             bool isSum = false;
             for (int i = 0; i < numbers.Length; i++)
@@ -16,16 +16,18 @@ namespace _04TripleSum
                 for (int j = i + 1; j < numbers.Length; j++)
                 {
                     int sum = numbers[i] + numbers[j];
-                        if (numbers.Contains(sum))
-                        {
-                            Console.WriteLine($"{numbers[i]} + {numbers[j]} == {sum}");
-                            isSum = true;
-                        }                    
+                    if (numbers.Contains(sum))
+                    {
+                        Console.WriteLine($"{numbers[i]} + {numbers[j]} == {sum}");
+                        isSum = true;
+                    }                    
                 }
             }
 
             if (!isSum)
+            {
                 Console.WriteLine("No");
+            }
         }
     }
 }
