f2854ee940c806acfae7dc6e6b5994d162f2704d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-01-09 14:57:17 +0200

    Update 07CountNumbers.cs

--- 07ProgrammingFundamentals_ListsLab/07CountNumbers.cs
+++ 07ProgrammingFundamentals_ListsLab/07CountNumbers.cs
@@ -8,13 +8,10 @@ namespace _07CountNumbers
     {
         static void Main(string[] args)
         {
-            List<int>numbers = Console.ReadLine()
-                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToList();
+            List<int>numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
             
             int[] counts = new int[numbers.Max() + 1];
-
             foreach (int number in numbers)
             {
                 counts[number]++;
