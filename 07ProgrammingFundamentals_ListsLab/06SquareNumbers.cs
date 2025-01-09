d1a22cac616e1d11b98f516a4f6217a5ab0c8b60
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-01-09 14:54:25 +0200

    Update 06SquareNumbers.cs

--- 07ProgrammingFundamentals_ListsLab/06SquareNumbers.cs
+++ 07ProgrammingFundamentals_ListsLab/06SquareNumbers.cs
@@ -8,12 +8,10 @@ namespace _06SquareNumbers
     {
         static void Main(string[] args)
         {
-            List<int> numbers = Console.ReadLine()
-                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToList();
+            List<int> numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+            
             List<int> squareNumbers = new List<int>();
-
             for (int i = 0; i < numbers.Count; i++)
             {
                 if (Math.Sqrt(numbers[i]) == (int)Math.Sqrt(numbers[i]))
