63d650f849da282bd82209f57f24769d8b9cc335
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-01-11 18:48:03 +0200

    Update 01CountRealNumbers.cs

--- 08ProgrammingFundamentals_DictionariesLambdaExpressionsAndLINQLab/01CountRealNumbers.cs
+++ 08ProgrammingFundamentals_DictionariesLambdaExpressionsAndLINQLab/01CountRealNumbers.cs
@@ -8,8 +8,7 @@ namespace _01CountRealNumbers
     {
         static void Main(string[] args)
         {
-            double[] numbers = Console.ReadLine()
-                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            double[] numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(double.Parse).ToArray();
             SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
             foreach (double number in numbers)
