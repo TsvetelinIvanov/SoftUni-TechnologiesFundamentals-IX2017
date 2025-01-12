d49f869115fcf8d15a85c74e6016d41d747360e6
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-01-12 20:11:43 +0200

    Update 07AndreyAndBilliard.cs

--- 09ProgrammingFundamentals_ObjectsAndClassesExercise/07AndreyAndBilliard.cs
+++ 09ProgrammingFundamentals_ObjectsAndClassesExercise/07AndreyAndBilliard.cs
@@ -7,7 +7,9 @@ namespace _07AndreyAndBilliard
     class Customer
     {
         public string Name { get; set; }
+        
         public Dictionary<string, int> ShopList { get; set; }
+        
         public decimal Bill { get; set; }
     }
 
@@ -16,8 +18,8 @@ namespace _07AndreyAndBilliard
         static void Main(string[] args)
         {
             Dictionary<string, decimal> prices = new Dictionary<string, decimal>();
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 0; i < n; i++)
+            int pricesCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < pricesCount; i++)
             {
                 string[] priceInfo = Console.ReadLine().Split('-');
                 string product = priceInfo[0];
@@ -35,7 +37,7 @@ namespace _07AndreyAndBilliard
             while ((order = Console.ReadLine()) != "end of clients")
             {
                 string[] orderInfo = order.Split('-', ',');
-                string client = orderInfo[0];
+                string customerName = orderInfo[0];
                 string product = orderInfo[1];
                 int quantity = int.Parse(orderInfo[2]);
 
@@ -44,9 +46,9 @@ namespace _07AndreyAndBilliard
                     continue;
                 }
 
-                if (customers.Any(c => c.Name == client))
+                if (customers.Any(c => c.Name == customerName))
                 {
-                    Customer customer = customers.Single(c => c.Name == client);
+                    Customer customer = customers.Single(c => c.Name == customerName);
                     if (!customer.ShopList.ContainsKey(product))
                     {
                         customer.ShopList.Add(product, 0);
@@ -58,10 +60,12 @@ namespace _07AndreyAndBilliard
                 else
                 {
                     Customer customer = new Customer();
-                    customer.Name = client;
+                    customer.Name = customerName;
                     customer.ShopList = new Dictionary<string, int>();
+                    
                     customer.ShopList[product] = quantity;
                     customer.Bill = prices[product] * quantity;
+                    
                     customers.Add(customer);
                 }                
             }
