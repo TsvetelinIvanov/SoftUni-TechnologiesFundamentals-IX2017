5264a41528fce1742a0f8265e075d2bd010c76e7
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-01-14 13:55:06 +0200

    Update 06RectanglePosition.cs

--- 09ProgrammingFundamentals_ObjectsAndClassesLab/06RectanglePosition.cs
+++ 09ProgrammingFundamentals_ObjectsAndClassesLab/06RectanglePosition.cs
@@ -6,22 +6,33 @@ namespace _06RectanglePosition
 {
     class Rectangle
     {
-        public int Top { get; set; }
         public int Left { get; set; }
+        
+        public int Bottom { get; set; }
+        
         public int Width { get; set; }
+        
         public int Hight { get; set; }
-        public int Right { get { return Left + Width; } }
-        public int Bottom { get { return Top + Hight; } }
-
-        public bool IsInside(Rectangle other)
+        
+        public int Right 
         {
-            bool isInLeft = Left >= other.Left;
-            bool isInRight = Right <= other.Right;
-            bool isInTop = Top >= other.Top;
-            bool isInBottom = Bottom <= other.Bottom;
+            get { return Left + Width; }
+        }
+        
+        public int Top
+        {
+            get { return Bottom + Hight; }
+        }
+
+        public bool IsInside(Rectangle otherRectangle)
+        {
+            bool isInLeft = Left >= otherRectangle.Left;
+            bool isInRight = Right <= otherRectangle.Right;
+            bool isInBottom = Bottom >= otherRectangle.Bottom;
+            bool isInTop = Top <= other.Top;
 
             bool isInsideHorizontal = isInLeft && isInRight;
-            bool isInsideVertical = isInTop && isInBottom;
+            bool isInsideVertical = isInBottom && isInTop;
             bool isInside = isInsideHorizontal && isInsideVertical;
 
             return isInside;
@@ -32,10 +43,10 @@ namespace _06RectanglePosition
     {
         static void Main(string[] args)
         {
-            Rectangle rectangle1 = ReadRectangle();
-            Rectangle rectangle2 = ReadRectangle();
+            Rectangle firstRectangle = ReadRectangle();
+            Rectangle secondRectangle = ReadRectangle();
 
-            if (rectangle1.IsInside(rectangle2))
+            if (firstRectangle.IsInside(secondRectangle))
             {
                 Console.WriteLine("Inside");
             }
@@ -51,7 +62,7 @@ namespace _06RectanglePosition
             Rectangle rectangle = new Rectangle()
             {
                 Left = size.First(),
-                Top = size.Skip(1).First(),
+                Bottom = size.Skip(1).First(),
                 Width = size.Skip(2).First(),
                 Hight = size.Skip(3).First()
             };
