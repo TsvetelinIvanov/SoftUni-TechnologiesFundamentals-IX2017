05677336054cb77a968af0533d53ba64b608015e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-01-15 09:08:00 +0200

    Update 08AverageGrades.cs

--- 10ProgrammingFundamentals_FilesAndExceptionsExercise/08AverageGrades.cs
+++ 10ProgrammingFundamentals_FilesAndExceptionsExercise/08AverageGrades.cs
@@ -8,7 +8,9 @@ namespace _08AverageGrades
     class Student
     {
         public string Name { get; set; }
+        
         public List<double> Grades { get; set; }
+        
         public double Average => Grades.Average();
     }
 
@@ -16,28 +18,29 @@ namespace _08AverageGrades
     {
         static void Main(string[] args)
         {
-            StreamReader file = new StreamReader("input.txt");
-            using (file)
+            StreamReader streamReader = new StreamReader("input.txt");
+            using (streamReader)
             {
                 List<Student> students = new List<Student>();
-                int n = int.Parse(file.ReadLine());
-                string[] inputArguments = new string[n];
-                for (int i = 0; i < n; i++)
+                int studentsCount = int.Parse(streamReader.ReadLine());
+                string[] studentInfo = new string[studentsCount];
+                for (int i = 0; i < studentsCount; i++)
                 {
                     Student student = new Student();
-                    inputArguments = file.ReadLine().Split(new char[]
-                    { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
-                    student.Name = inputArguments[0];
-                    student.Grades = inputArguments.Skip(1).Select(double.Parse).ToList();
+                    
+                    studentInfo = streamReader.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    student.Name = studentInfo[0];
+                    student.Grades = studentInfo.Skip(1).Select(double.Parse).ToList();
+                    
                     students.Add(student);
                 }
 
-                string text = string.Empty;
-                students.Where(s => s.Average >= 5.00).OrderBy(s => s.Name)
-                    .ThenByDescending(s => s.Average).ToList()
-                    .ForEach(s => text += $"{s.Name} -> {s.Average:f2}\r\n");
+                string output = string.Empty;
+                students.Where(s => s.Average >= 5.00)
+                    .OrderBy(s => s.Name).ThenByDescending(s => s.Average)
+                    .ToList().ForEach(s => output += $"{s.Name} -> {s.Average:f2}\r\n");
 
-                File.WriteAllText("output.txt", text);
+                File.WriteAllText("output.txt", output);
             }
         }
     }
