4fa8b0bca8975e6ecf7dcecb8a255745602870c6
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-01-15 09:32:13 +0200

    Update 09BookLibrary.cs

--- 10ProgrammingFundamentals_FilesAndExceptionsExercise/09BookLibrary.cs
+++ 10ProgrammingFundamentals_FilesAndExceptionsExercise/09BookLibrary.cs
@@ -6,31 +6,38 @@ using System.Linq;
 
 namespace _09BookLibrary
 {
-    class Library
-    {
-        string Name { get; set; }
-        public List<Book> Books { get; set; }
-    }
-
     class Book
     {
         public string Title { get; set; }
+        
         public string Author { get; set; }
+        
         public string Publisher { get; set; }
+        
         public DateTime ReleaseDate { get; set; }
+        
         public string ISBN { get; set; }
+        
         public decimal Price { get; set; }
+    }
+    
+    class Library
+    {
+        public string Name { get; set; }
+        
+        public List<Book> Books { get; set; }
     }
 
     class Program
     {
-        static StreamReader file = new StreamReader("input.txt");
+        static StreamReader streamReader = new StreamReader("input.txt");
+        
         static void Main(string[] args)
         {            
-            int numOfBooks = int.Parse(file.ReadLine());
+            int booksCount = int.Parse(streamReader.ReadLine());
             Library library = new Library();
             List<Book> books = new List<Book>();
-            for (int i = 0; i < numOfBooks; i++)
+            for (int i = 0; i < booksCount; i++)
             {
                 Book book = ReadBook();
                 books.Add(book);
@@ -39,15 +46,13 @@ namespace _09BookLibrary
             library.Books = books;
             PrintBooks(library);
 
-            file.Close();
+            streamReader.Close();
         }
 
         private static Book ReadBook()
         {
-
             Book book = new Book();
-            string[] bookInfo = file.ReadLine().Split(new char[] { ' ' },
-                StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string[] bookInfo = streamReader.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
             book.Title = bookInfo[0];
             book.Author = bookInfo[1];
             book.Publisher = bookInfo[2];
@@ -60,15 +65,20 @@ namespace _09BookLibrary
 
         private static void PrintBooks(Library library)
         {
-            var ordered = library.Books.GroupBy(b => b.Author)
-                .Select(g => new { Author = g.Key, Prices = g.Sum(s => s.Price) })
+            var orderedAuthorsAndPrices = library.Books.GroupBy(b => b.Author)
+                .Select(g => new
+                        { 
+                            Author = g.Key,
+                            Prices = g.Sum(b => b.Price) 
+                        })
                 .OrderByDescending(x => x.Prices)
                 .ThenBy(x => x.Author)
                 .ToList();
+            
             string text = string.Empty;
-            foreach (var author in ordered)
+            foreach (var authorAndPrices in orderedAuthorsAndPrices)
             {
-                text += ($"{author.Author} -> {author.Prices:f2}\r\n");
+                text += ($"{authorAndPrices.Author} -> {authorAndPrices.Prices:f2}\r\n");
             }
 
             File.WriteAllText("output.txt", text);
