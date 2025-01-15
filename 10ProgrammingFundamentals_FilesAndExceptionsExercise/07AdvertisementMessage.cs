5bfa65af1eb0bd4bdc09e9cdd857df63a83fe13d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-01-15 08:54:41 +0200

    Update 07AdvertisementMessage.cs

--- 10ProgrammingFundamentals_FilesAndExceptionsExercise/07AdvertisementMessage.cs
+++ 10ProgrammingFundamentals_FilesAndExceptionsExercise/07AdvertisementMessage.cs
@@ -9,19 +9,20 @@ namespace _07AdvertisementMessage
         {
             string[] phrases = { "Excellent product.", "Such a great product.", "I always use that product.",
                 "Best product of its category.", "Exceptional product.", "I can’t live without this product." };
-            string[] events = { "Now I feel good.", "I have succeeded with this product.",
-                "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.",
-                "Try it yourself, I am very satisfied.", "I feel great!" };
+            string[] events = { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
             string[] autors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
             string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
 
-            int n = int.Parse(File.ReadAllText("input.txt"));
+            int advertisementsCount = int.Parse(File.ReadAllText("input.txt"));
+            
             Random phraseIndex = new Random();
             Random eventIndex = new Random();
             Random autorIndex = new Random();
             Random cityIndex = new Random();
+            
             string text = string.Empty;
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < advertisementsCount; i++)
             {
                text += ($"{phrases[phraseIndex.Next(phrases.Length)]} " +
                     $"{events[eventIndex.Next(events.Length)]} {autors[autorIndex.Next(autors.Length)]} - " +
