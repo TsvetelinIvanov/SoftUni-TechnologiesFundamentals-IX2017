3130454604684b1810b8bcd1816027cc411e614d
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-01-21 22:07:18 +0200

    Update 02MatchPhoneNumber.cs

--- 12ProgrammingFundamentals_RegularExpressionsLab/02MatchPhoneNumber.cs
+++ 12ProgrammingFundamentals_RegularExpressionsLab/02MatchPhoneNumber.cs
@@ -10,6 +10,7 @@ namespace _02MatchPhoneNumber
         {
             Regex regex = new Regex(@"(^| )\+359(-| )2\2\d{3}\2\d{4}\b");
             string name = Console.ReadLine();
+            
             MatchCollection matches = regex.Matches(name);
             string[] result = matches.Cast<Match>().Select(x => x.Value.Trim()).ToArray();
             Console.WriteLine(string.Join(", ", result));
