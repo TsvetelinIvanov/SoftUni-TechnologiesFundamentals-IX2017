06428a24283f25d785d567db323928574bcfd5cf
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-01-27 20:55:00 +0200

    Update 03FootballLeague.cs

--- ExamProgrammingFundamentals12VI2016/03FootballLeague.cs
+++ ExamProgrammingFundamentals12VI2016/03FootballLeague.cs
@@ -16,79 +16,81 @@ namespace _03FootballLeague
             while ((footballInfo = Console.ReadLine()) != "final")
             {                
                 string[] teamsAndScores = footballInfo.Split(new string[] { key }, StringSplitOptions.None);
-                string team1 = teamsAndScores[1].ToUpper();
-                team1 = Reverse(team1);
-                string team2 = teamsAndScores[3].ToUpper();
-                team2 = Reverse(team2);
+                string firstTeam = teamsAndScores[1].ToUpper();
+                firstTeam = Reverse(firstTeam);
+                
+                string secondTeam = teamsAndScores[3].ToUpper();
+                secondTeam = Reverse(secondTeam);
+                
                 string[] scores = teamsAndScores[4].Split(new char[] { ' ', ':' });
-                long score1 = 0;
-                long score2 = 0;
+                long firstTeamScore = 0;
+                long secondTeamScore = 0;
                 if (scores.Length == 2)
                 {
-                    score1 = long.Parse(scores[0]);
-                    score2 = long.Parse(scores[1]);
+                    firstTeamScore = long.Parse(scores[0]);
+                    secondTeamScore = long.Parse(scores[1]);
                 }
                 else if (scores.Length == 3)
                 {
-                    score1 = long.Parse(scores[1]);
-                    score2 = long.Parse(scores[2]);
+                    firstTeamScore = long.Parse(scores[1]);
+                    secondTeamScore = long.Parse(scores[2]);
                 }
 
-                int points1 = 0;
-                int points2 = 0;
-                if (score1 == score2)
+                int firstTeamPointsCount = 0;
+                int secondTeamPointsCount = 0;
+                if (firstTeamScore == secondTeamScore)
                 {
-                    points1 = 1;
-                    points2 = 1;
+                    firstTeamPointsCount = 1;
+                    secondTeamPointsCount = 1;
                 }
-                else if (score1 > score2)
+                else if (firstTeamScore > secondTeamScore)
                 {
-                    points1 = 3;
+                    firstTeamPointsCount = 3;
                 }
-                else if (score1 < score2)
+                else if (firstTeamScore < secondTeamScore)
                 {
-                    points2 = 3;
+                    secondTeamPointsCount = 3;
                 }
 
-                if (!teamsByPoints.ContainsKey(team1))
+                if (!teamsByPoints.ContainsKey(firstTeam))
                 {
-                    teamsByPoints[team1] = 0;
+                    teamsByPoints[firstTeam] = 0;
                 }
 
-                teamsByPoints[team1] += points1;
+                teamsByPoints[firstTeam] += firstTeamPointsCount;
 
-                if (!teamsByPoints.ContainsKey(team2))
+                if (!teamsByPoints.ContainsKey(secondTeam))
                 {
-                    teamsByPoints[team2] = 0;
+                    teamsByPoints[secondTeam] = 0;
                 }
 
-                teamsByPoints[team2] += points2;
+                teamsByPoints[secondTeam] += secondTeamPointsCount;
 
-                if (!teamsByGoals.ContainsKey(team1))
+                if (!teamsByGoals.ContainsKey(firstTeam))
                 {
-                    teamsByGoals.Add(team1, 0);
+                    teamsByGoals.Add(firstTeam, 0);
                 }
 
-                teamsByGoals[team1] += score1;
+                teamsByGoals[firstTeam] += firstTeamScore;
 
-                if (!teamsByGoals.ContainsKey(team2))
+                if (!teamsByGoals.ContainsKey(secondTeam))
                 {
-                    teamsByGoals.Add(team2, 0);
+                    teamsByGoals.Add(secondTeam, 0);
                 }
 
-                teamsByGoals[team2] += score2;
+                teamsByGoals[secondTeam] += secondTeamScore;
             }
 
             Console.WriteLine("League standings:");
             int place = 0;
-            foreach (KeyValuePair<string, int> team in teamsByPoints.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            foreach (KeyValuePair<string, int> team in teamsByPoints.OrderByDescending(t => t.Value).ThenBy(t => t.Key))
             {
                 place++;
                 Console.WriteLine("{0}. {1} {2}", place, team.Key, team.Value);
             }
 
             Console.WriteLine("Top 3 scored goals:");
-            foreach (KeyValuePair<string, long> team in teamsByGoals.OrderByDescending(x => x.Value).ThenBy(x => x.Key).Take(3))
+            foreach (KeyValuePair<string, long> team in teamsByGoals.OrderByDescending(t => t.Value).ThenBy(t => t.Key).Take(3))
             {
                 Console.WriteLine($"- {team.Key} -> {team.Value}");                
             }            
@@ -96,10 +98,10 @@ namespace _03FootballLeague
 
         private static string Reverse(string team)
         {
-            char[] teamChar = team.ToCharArray();
-            Array.Reverse(teamChar);
+            char[] teamCharacters = team.ToCharArray();
+            Array.Reverse(teamCharacters);
             string reversedTeam = string.Empty;
-            foreach (char ch in teamChar)
+            foreach (char ch in teamCharacters)
             {
                 reversedTeam += ch;
             }
