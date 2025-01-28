aaff0f752fa6278b1dffcde68b5d88ab5cad5991
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-01-28 21:09:05 +0200

    Update 04RoliTheCoder.cs

--- ExamProgrammingFundamentals23X2016/04RoliTheCoder.cs
+++ ExamProgrammingFundamentals23X2016/04RoliTheCoder.cs
@@ -8,30 +8,29 @@ namespace _04RoliTheCoder
     {
         static void Main(string[] args)
         {
-            Dictionary<string, Dictionary<string, SortedSet<string>>> eventsByIDs =
-                new Dictionary<string, Dictionary<string, SortedSet<string>>>();
+            Dictionary<string, Dictionary<string, SortedSet<string>>> eventsByIds = new Dictionary<string, Dictionary<string, SortedSet<string>>>();
             string input = string.Empty;
             while ((input = Console.ReadLine()) != "Time for Code")
             {
                 string[] requests = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);          
                 if (requests.Length >= 2 && requests[1].StartsWith("#")) 
                 {
-                    string ID = requests[0];
+                    string Id = requests[0];
                     string eventName = requests[1].Remove(0, 1);
-                    if (!eventsByIDs.ContainsKey(ID))
+                    if (!eventsByIds.ContainsKey(Id))
                     {
-                        eventsByIDs[ID] = new Dictionary<string, SortedSet<string>>();
-                        eventsByIDs[ID][eventName] = new SortedSet<string>();
+                        eventsByIds[Id] = new Dictionary<string, SortedSet<string>>();
+                        eventsByIds[Id][eventName] = new SortedSet<string>();
                         for (int i = 2; i < requests.Length; i++)
                         {
-                            eventsByIDs[ID][eventName].Add(requests[i]);
+                            eventsByIds[Id][eventName].Add(requests[i]);
                         }
                     }
-                    else if (eventsByIDs[ID].ContainsKey(eventName))
+                    else if (eventsByIds[Id].ContainsKey(eventName))
                     {
                         for (int i = 2; i < requests.Length; i++)
                         {
-                            eventsByIDs[ID][eventName].Add(requests[i]);
+                            eventsByIds[Id][eventName].Add(requests[i]);
                         }
                     }
                 }
