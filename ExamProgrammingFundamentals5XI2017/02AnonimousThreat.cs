c13158fc59ac562712e48a83fb6f1b3da0075981
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-01-30 16:27:16 +0200

    Update 02AnonimousThreat.cs

--- ExamProgrammingFundamentals5XI2017/02AnonimousThreat.cs
+++ ExamProgrammingFundamentals5XI2017/02AnonimousThreat.cs
@@ -8,39 +8,42 @@ namespace _02AnonimousThreat
     {
         static void Main(string[] args)
         {
-            List<string> data = Console.ReadLine()
-                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                .ToList();            
+            List<string> data = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();            
             string commandLine = Console.ReadLine();
             while (commandLine != "3:1")
             {
-                string[] commands = commandLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                if (commands[0] == "merge")
+                string[] commandData = commandLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (commandData[0] == "merge")
                 {
-                    int startIndex = int.Parse(commands[1]);
-                    int endIndex = int.Parse(commands[2]);
-                    string sumString = string.Empty;
+                    int startIndex = int.Parse(commandData[1]);
+                    int endIndex = int.Parse(commandData[2]);
+                    string dataForMerge = string.Empty;
                     if (startIndex < 0 || startIndex > data.Count - 1)
+                    {
                         startIndex = 0;
+                    }
 
                     if (endIndex < 0 || endIndex > data.Count - 1)
+                    {
                         endIndex = data.Count - 1;
+                    }
 
                     for (int i = startIndex; i <= endIndex; i++)
                     {
-                        sumString += data[i];
+                        dataForMerge += data[i];
                     }
 
                     data.RemoveRange(startIndex, endIndex - startIndex + 1);
-                    data.Insert(startIndex, sumString);
+                    data.Insert(startIndex, dataForMerge);
                 }
-                else if (commands[0] == "divide")
+                else if (commandData[0] == "divide")
                 {
-                    int index = int.Parse(commands[1]);
-                    int partitionsCount = int.Parse(commands[2]);
-                    List<string> resultByDividing = DivideElement(data[index], partitionsCount);
+                    int index = int.Parse(commandData[1]);
+                    int partitionsCount = int.Parse(commandData[2]);
+                    List<string> elementPartitions = DivideElement(data[index], partitionsCount);
+                    
                     data.RemoveAt(index);
-                    data.InsertRange(index, resultByDividing);
+                    data.InsertRange(index, elementPartitions);
                 }
 
                 commandLine = Console.ReadLine();
@@ -49,14 +52,14 @@ namespace _02AnonimousThreat
             Console.WriteLine(string.Join(" ", data));
         }
 
-        private static List<string> DivideElement(string word, int divide)
+        private static List<string> DivideElement(string word, int divisor)
         {
-            int partitionLength = word.Length / divide;
+            int partitionLength = word.Length / divisor;
             List<string> resultByDividing = new List<string>();            
-            for (int i = 0; i < divide; i++)
+            for (int i = 0; i < divisor; i++)
             {
-                string element = word.Substring(0, partitionLength);
-                resultByDividing.Add(element);
+                string partition = word.Substring(0, partitionLength);
+                resultByDividing.Add(partition);
                 word = word.Substring(partitionLength);
             }
 
