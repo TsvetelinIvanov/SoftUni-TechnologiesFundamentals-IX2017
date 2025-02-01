8ff8f410b0c61634e44f729a6f6939c40c8cdf04
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-02-01 22:31:17 +0200

    Update 05WormTest.cs

--- Extended01ProgrammingFundamentals_DataTypesVariablesAndMethodsExercise/05WormTest.cs
+++ Extended01ProgrammingFundamentals_DataTypesVariablesAndMethodsExercise/05WormTest.cs
@@ -9,7 +9,7 @@ namespace _05WormTest
             int wormLengthInMeters = int.Parse(Console.ReadLine());
             double wormWidthInCentimeters = double.Parse(Console.ReadLine());
             double wormLengthInCentimeters = wormLengthInMeters * 100;
-            if (wormLengthInCentimeters % wormWidthInCentimeters == 0 || wormWidthInCentimeters == 0)
+            if (wormWidthInCentimeters == 0 || wormLengthInCentimeters % wormWidthInCentimeters == 0)
             {
                 Console.WriteLine("{0:f2}", wormLengthInCentimeters * wormWidthInCentimeters);
             }
