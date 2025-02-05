3ccb6324aa5bb6f5a72af0db7107fac383369337
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-02-05 23:59:26 +0200

    Update 02CountSubstringOccurrences.cs

--- Extended03ProgrammingFundamentals_StringsAndRegularExpressionsLab/02CountSubstringOccurrences.cs
+++ Extended03ProgrammingFundamentals_StringsAndRegularExpressionsLab/02CountSubstringOccurrences.cs
@@ -8,13 +8,14 @@ namespace _02CountSubstringOccurrences
         {
             string text = Console.ReadLine().ToLower();
             string pattern = Console.ReadLine().ToLower();
+            
             int counter = 0;
             int index = text.IndexOf(pattern);
             while (index != - 1)
             {
                 counter++;
                 index = text.IndexOf(pattern, index + 1);
-                //if you wont to count only the whole occurences:
+                //If you wont to count only the whole occurences:
                 //index = text.IndexOf(pattern, index + pattern.Length);
             }
 
