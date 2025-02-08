f228097e29a6933d116e8a4535b9ad662c3ca4cc
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-02-08 14:05:45 +0200

    Update 03CitiesByContinentAndCountry.cs

--- Extended04ProgrammingFundamentals_DictionariesLINQandLambdaExpressionsLab/03CitiesByContinentAndCountry.cs
+++ Extended04ProgrammingFundamentals_DictionariesLINQandLambdaExpressionsLab/03CitiesByContinentAndCountry.cs
@@ -7,37 +7,36 @@ namespace _03CitiesByContinentAndCountry
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            Dictionary<string, Dictionary<string, List<string>>> citiesByCountriesAndContinents =
-                new Dictionary<string, Dictionary<string, List<string>>>();
-            for (int i = 0; i < n; i++)
+            int dataPiecesCount = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> citiesByCountriesAndContinents = new Dictionary<string, Dictionary<string, List<string>>>();
+            for (int i = 0; i < dataPiecesCount; i++)
             {
-                string[] data = Console.ReadLine()
-                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] data = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
                 string continent = data[0];
                 string country = data[1];
                 string city = data[2];
+                
                 if (!citiesByCountriesAndContinents.ContainsKey(continent))
                 {
                     citiesByCountriesAndContinents.Add(continent, new Dictionary<string, List<string>>());
-                    //or citiesByCountriesAndContinents[continent] = new Dictionary<string, List<string>>();
+                    //citiesByCountriesAndContinents[continent] = new Dictionary<string, List<string>>();
                 }
 
                 if (!citiesByCountriesAndContinents[continent].ContainsKey(country))
                 {
-                    citiesByCountriesAndContinents[continent][country] = new List<string>();
-                    //or citiesByCountriesAndContinents[continent].Add(country, new List<string>());
+                    citiesByCountriesAndContinents[continent].Add(country, new List<string>());
+                    //citiesByCountriesAndContinents[continent][country] = new List<string>();
                 }
 
                 citiesByCountriesAndContinents[continent][country].Add(city);                
             }
 
-            foreach (KeyValuePair<string, Dictionary<string, List<string>>> citiesByCountriesInContinent in citiesByCountriesAndContinents)
+            foreach (KeyValuePair<string, Dictionary<string, List<string>>> citiesByCountriesInAContinent in citiesByCountriesAndContinents)
             {
-                Console.WriteLine(citiesByCountriesInContinent.Key + ":");
-                foreach (KeyValuePair<string, List<string>> citiesInCountry in citiesByCountriesInContinent.Value)
+                Console.WriteLine(citiesByCountriesInAContinent.Key + ":");
+                foreach (KeyValuePair<string, List<string>> citiesInACountry in citiesByCountriesInAContinent.Value)
                 {
-                    Console.WriteLine($"  {citiesInCountry.Key} -> {string.Join(", ", citiesInCountry.Value)}");
+                    Console.WriteLine($"  {citiesInACountry.Key} -> {string.Join(", ", citiesInACountry.Value)}");
                 }
             }
         }
