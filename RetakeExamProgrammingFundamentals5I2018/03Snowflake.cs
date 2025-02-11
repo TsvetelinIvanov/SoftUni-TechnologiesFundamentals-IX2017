36c00cad8edfa84be567647f53b29236ff70f5f2
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-02-11 10:46:02 +0200

    Update 03Snowflake.cs

--- RetakeExamProgrammingFundamentals5I2018/03Snowflake.cs
+++ RetakeExamProgrammingFundamentals5I2018/03Snowflake.cs
@@ -7,51 +7,51 @@ namespace _03Snowflake
     {
         static void Main(string[] args)
         {
-            string patternSurface = $"^([^a-zA-Z0-9]+)$";
-            string patternMantle = $"^([0-9_]+)$";
-            string patternCore = $"^([a-zA-Z])$";
-            string patternMiddle = $"^([^a-zA-Z0-9]+)([0-9_]+)([a-zA-Z]+)([0-9_]+)([^a-zA-Z0-9]+)$";
+            string surfacePattern = $"^([^a-zA-Z0-9]+)$";
+            string mantlePattern = $"^([0-9_]+)$";
+            //string corePattern = $"^([a-zA-Z])$";
+            string middlePattern = $"^([^a-zA-Z0-9]+)([0-9_]+)([a-zA-Z]+)([0-9_]+)([^a-zA-Z0-9]+)$";
             bool isValid = true;
 
-            string input1 = Console.ReadLine();
-            Match match1 = Regex.Match(input1, patternSurface);
-            if (!match1.Success)
+            string surfaceInput = Console.ReadLine();
+            Match surfaceMatch = Regex.Match(surfaceInput, surfacePattern);
+            if (!surfaceMatch.Success)
             {
                 isValid = false;
             }
 
-            string input2 = Console.ReadLine();
-            Match match2 = Regex.Match(input2, patternMantle);
-            if (!match2.Success)
+            string mantleInput = Console.ReadLine();
+            Match mantleMatch = Regex.Match(mantleInput, mantlePattern);
+            if (!mantleMatch.Success)
             {
                 isValid = false;
-            }                
+            }
 
-            string input3 = Console.ReadLine();
-            Match match3 = Regex.Match(input3, patternMiddle);
-            if (!match3.Success)
+            string middleInput = Console.ReadLine();
+            Match middleMatch = Regex.Match(middleInput, middlePattern);
+            if (!middleMatch.Success)
             {
                 isValid = false;
-            }                
+            }
 
-            string input4 = Console.ReadLine();
-            Match match4 = Regex.Match(input4, patternMantle);
-            if (!match4.Success)
+            string anotherMantleInput = Console.ReadLine();
+            Match anotherMantleMatch = Regex.Match(anotherMantleInput, mantlePattern);
+            if (!anotherMantleMatch.Success)
             {
                 isValid = false;
-            }                
+            }
 
-            string input5 = Console.ReadLine();
-            Match match5 = Regex.Match(input5, patternSurface);
-            if (!match5.Success)
+            string anotherSurfaceInput = Console.ReadLine();
+            Match anotherSurfaceMatch = Regex.Match(anotherSurfaceInput, surfacePattern);
+            if (!anotherSurfaceMatch.Success)
             {
                 isValid = false;
-            }                
+            }
 
             if (isValid)
             {
                 Console.WriteLine("Valid");
-                Console.WriteLine(match3.Groups[3].Length);
+                Console.WriteLine(middleMatch.Groups[3].Length);
             }
             else
             {
