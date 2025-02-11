42e2a32c6d486cc4e4375b9d4c657321b7bfb766
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2025-02-11 11:07:53 +0200

    Update 04_Snowwhite.cs

--- RetakeExamProgrammingFundamentals5I2018/04_Snowwhite.cs
+++ RetakeExamProgrammingFundamentals5I2018/04_Snowwhite.cs
@@ -7,7 +7,9 @@ namespace _04Snowwhite
     class Dwarf
     {
         public string Name { get; set; }
+        
         public string HatColor { get; set; }
+        
         public int Physics { get; set; }
     }
 
@@ -19,10 +21,10 @@ namespace _04Snowwhite
             string input = Console.ReadLine();
             while (input != "Once upon a time")
             {
-                string[] dwarfsData = input.Split(new string[] { " <:> " }, StringSplitOptions.None);
-                string dwarfName = dwarfsData[0];
-                string dwarfHatColor = dwarfsData[1];
-                int dwarfPhysics = int.Parse(dwarfsData[2]);                
+                string[] dwarfData = input.Split(new string[] { " <:> " }, StringSplitOptions.None);
+                string dwarfName = dwarfData[0];
+                string dwarfHatColor = dwarfData[1];
+                int dwarfPhysics = int.Parse(dwarfData[2]);                
                 
                 if (!dwarfs.Any(d => d.Name == dwarfName && d.HatColor == dwarfHatColor))
                 {
@@ -47,8 +49,7 @@ namespace _04Snowwhite
                 input = Console.ReadLine();
             }
            
-            foreach (Dwarf dwarf in dwarfs.OrderByDescending(d => d.Physics)
-                .ThenByDescending(d => dwarfs.Where(dw => dw.HatColor == d.HatColor).Count()))
+            foreach (Dwarf dwarf in dwarfs.OrderByDescending(d => d.Physics).ThenByDescending(d => dwarfs.Where(dw => dw.HatColor == d.HatColor).Count()))
             {
                Console.WriteLine($"({dwarf.HatColor}) {dwarf.Name} <-> {dwarf.Physics}");
             }
